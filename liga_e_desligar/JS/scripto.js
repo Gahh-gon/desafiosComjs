03acc208c612f930716b7e38ee20a086a568bcdd
Gahh-gon <77693978+Gahh-gon@users.noreply.github.com>
2022-01-30 21:57:37 -0300

    dois projetos 60%

---
+++ liga_e_desligar/JS/scripto.js
@@ -0,0 +1,27 @@
+
+
+
+ var fundo = document.getElementById("fundo");
+ 
+
+function desligar(){
+
+    document.getElementById("img").src="../IMG/lampada_Desligado.png";
+
+ fundo.style.backgroundColor = '#000';
+  
+  
+   
+   
+   }
+
+   function ligar(){
+
+    document.getElementById("img").src="../IMG/lampada_ligada.png";
+   
+
+
+
+
+   }
+   
